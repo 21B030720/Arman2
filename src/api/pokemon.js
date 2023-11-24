54b9567afade6baf51186a4b8e41a34ee1b339f7
Arslan Toimbekov <98741201+21B030720@users.noreply.github.com>
2023-11-24 20:29:07 +0600

    Add files via upload

---
+++ src/api/pokemon.js
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+const URL = `https://pokeapi.co/api/v2/pokemon?limit=151`;
+const URL2 = 'https://pokeapi.co/api/v2/pokemon?limit=14&offset=200'
+const URL3 = 'https://pokeapi.co/api/v2/pokemon?limit=14&offset=250'
+const URL4 = 'https://pokeapi.co/api/v2/pokemon?limit=15&offset=2'
+const URL5 = 'https://pokeapi.co/api/v2/pokemon?limit=14&offset=20'
+
+export const getAllPokemonList = async() => {
+    const {data} = await axios.get(URL5);
+    return data;
+}
